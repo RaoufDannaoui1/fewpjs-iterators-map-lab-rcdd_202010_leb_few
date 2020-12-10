7abd360d0dc1d913a77dd4cd463f5ff2e6102652
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2020-12-10 14:32:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+const right= [
+  "What Does The This Keyword Mean?",
+  "What Is The Contutorialuctor OO Pattern?",
+  "Implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What Is NaN And How Can We Check For It",
+  "What Is The Difference Between StopPropagation And PreventDefault?",
+  "Immutable State And Pure Functions",
+  "What Is The Difference Between == And ===?",
+  "What Is The Difference Between Event Capturing And Bubbling?",
+  "What Is JSONP?"
+]
 const titleCased = () => {
-  return tutorials
+  return right
 }
